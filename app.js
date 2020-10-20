c874e5286ffc28a1100d1355ed01b43e45d55174
Nero Angel <neroangel1909@gmail.com>
2020-10-20 01:17:45 -0500

    Repaso 1

---
+++ app.js
@@ -0,0 +1,23 @@
+const clickLocations = () => {
+    const locations = document.querySelector('.locations')
+    locations.scrollIntoView({behavior: "smooth", alignTo: true});
+}
+const clickBenefits = () => {
+    const benefits = document.querySelector('.benefits')
+    benefits.scrollIntoView({behavior: "smooth", alignTo: true});
+}
+const clickContact = () => {
+    const contact = document.querySelector('.contact')
+    contact.scrollIntoView({behavior: "smooth", alignTo: true});
+}
+window.onscroll = function(){
+     const y = this.scrollY;
+     const main_head = document.querySelector('.main-head');
+     if(y > 0){
+          main_head.style.opacity = "0.5";
+     }else{
+          main_head.style.opacity = "1";
+     }
+}
+
+
